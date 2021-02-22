8233d66efbb22168be744fc483cc2eb6c61b914f
younho <nnyyhh9247@gmail.com>
2021-02-22 19:02:34 +0900

    completed input reset

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-import getMyFavorite from './myFavoriteTitle';
-
-getMyFavorite();
-=======
 import fetchMyFavorite from './myFavoriteRender';
+import { resetTitle, generateName } from './generateName';
+
+generateName();
+resetTitle();
 
 document.addEventListener('DOMContentLoaded', fetchMyFavorite);
->>>>>>> 4c6e7e1b7fba52a955ae3c61bbc97ec209a9ae94
