6313fee602c044f8b193b1acd53a802f4898597d
bg-shorthand <hanhanhan333@gmail.com>
2021-02-23 17:27:55 +0900

    feat: selected item state

--- src/ts/chooseSection/chooseSectionMain.ts
+++ src/ts/chooseSection/chooseSectionMain.ts
@@ -1,7 +1,9 @@
 import openChooseModal from './openChooseModal'
 import closeChooseModal from './closeChooseModal'
+import makeSelectedItem from './selectedItemState';
 
 export default () => {
   openChooseModal();
   closeChooseModal();
+  makeSelectedItem();
 }
