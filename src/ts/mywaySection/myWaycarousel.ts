16e98563a48d147c9d6d2f4a85fe2957ae1d4ede
younho <nnyyhh9247@gmail.com>
2021-02-25 21:52:56 +0900

    fixed add favorite list

--- src/ts/mywaySection/myWaycarousel.ts
+++ src/ts/mywaySection/myWaycarousel.ts
@@ -5,14 +5,40 @@ const axios = require('axios');
 const carousel = async () => {
   const myFavoriteList = await axios.get('http://localhost:7000/myFavorite');
   
-  const listData = myFavoriteList.data;
+  const $carouselSlides = document.querySelector('.myway__list') as HTMLElement;
+  const $carouselControlNext = document.querySelector('.carousel-control.next') as HTMLButtonElement;
+  const $carouselControlPrev = document.querySelector('.carousel-control.prev') as HTMLButtonElement;
+  const listData = myFavoriteList.data;  
+  let currentSlide = 0;
+  // let numOfSlides = listData.length;
 
-  console.log(listData.length);
-  
+    if (listData.length > 4) {
+        $carouselControlNext?.addEventListener('click', () => {
 
-  listData.forEach((list: Category) => {
-    if (listData.length > 3) console.log(listData.length);
-  })
+          const carousel1 = async () => {
+            const myFavoriteList = await axios.get('http://localhost:7000/myFavorite');
+            let numOfSlides = myFavoriteList.data.length;
+            // let currentSlide = 0;
+          
+          if (currentSlide >= numOfSlides - 4) return $carouselControlNext.style.color = 'gray';
+
+          $carouselControlPrev.style.color = 'greenyellow';
+          currentSlide += 1;
+          $carouselSlides.style.setProperty('--currentSlide', currentSlide + '');
+          $carouselSlides.style.setProperty('--duration', '500');
+          }
+          carousel1();
+        });
+        
+        $carouselControlPrev.addEventListener('click', () => {
+          if (currentSlide <= 0) return $carouselControlPrev.style.color = 'gray';
+          
+          $carouselControlNext.style.color = 'greenyellow';
+          currentSlide -= 1;
+          $carouselSlides.style.setProperty('--currentSlide', currentSlide + '');
+          $carouselSlides.style.setProperty('--duration', '500');
+      });
+    }
 }
 
 export default carousel;
